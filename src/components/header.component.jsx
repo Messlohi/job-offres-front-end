7500c82a6fe5348b65b5a84b2c2d0e8763e26fd4
MohammedMesslohi <token.maroc@gmail.com>
2021-06-23 22:58:48 +0100

    adding node-sass

---
+++ src/components/header.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+import './header.styles.scss'
+
+
+const Header = () => {
+    
+    return(
+        <div className="classs">
+            Hello world
+        </div>
+    )
+}
+
+export default Header;

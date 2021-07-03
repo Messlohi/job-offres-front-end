a4d3d9e4403bbf34a48c1b140f10a45c098f26eb
SaidBelahrach <said.belahrach16@gmail.com>
2021-07-03 15:32:09 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import { Route } from 'react-router';
 import './App.css';
 import Header from './components/header/header.component';
 import Create_offre from './components/offre/Create_offre';
-import OffresPage from './components/offre/OffresPage';
-
+import OffresPage from './components/offre/OffresPage'; 
+import MainPage from './pages/main_page/main_page.component';
 function App() {
     return ( 
         <div>
             <Header/>
-            <div className="container  body-content content-wrapper">
-                {/* <MainPage/> */}
-                 <OffresPage/> 
-               {/* <Create_offre/>*/}
+            <Route path='/' exact component={MainPage} />
+            <div className="container  body-content content-wrapper"> 
+                <Route path='/offres' exact component={OffresPage} />
+                <Route path='/offres/add' exact  component={Create_offre} />
             </div>
         </div>
     );
